976720f35c5347376fef8d4bb7da439ae2c82543
eamonnofarrell <36230742+eamonnofarrell@users.noreply.github.com>
2018-03-07 11:28:29 +0000

    Add files via upload

---
+++ Week4_guessing.py
@@ -0,0 +1,19 @@
+#Eamonn O'Farrell
+# Suggested Week 4 tutorial - generate random number
+# Take user input to guess number
+
+from random import randint
+
+Target=randint(1, 100)
+Guess=101
+
+print("Guess a number between 1 and 100")
+while Guess != Target:
+    Guess=(int(input("Please enter your guess:")))
+    if Guess == Target:
+        print("You have guessed correctly!")
+    elif Guess<Target:
+        print("Too low!")
+    else:
+        print("Too high")
+            
