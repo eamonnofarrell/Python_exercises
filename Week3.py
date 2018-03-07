4c5e2f3d4ab4ade58f48d67685cea3a00f05277a
eamonnofarrell <36230742+eamonnofarrell@users.noreply.github.com>
2018-03-07 11:21:45 +0000

    Add files via upload

---
+++ Week3.py
@@ -0,0 +1,12 @@
+#Code by Eamonn O'Farrell 7/2/18
+#Collaz conjecture excercise
+
+n = int(input("Please enter an integer: "))
+
+while n>1:
+    if n%2 == 0:
+       n= n/2
+       print (n)
+    elif n/2  > 0:
+       n= (n*3) +1
+       print (n)
